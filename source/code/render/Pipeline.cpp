0c1e81808b11344c02c222f52867abed95a1a019
jayryness <jay.ryness@gmail.com>
2014-10-28 09:48:27 -0700

    split Stage.h out from Pipeline.h

--- source/code/render/Pipeline.cpp
+++ source/code/render/Pipeline.cpp
@@ -62,4 +62,50 @@ namespace eigen
         _filterStageAllocator.initialize(allocator, sizeof(FilterStage), 32);
     }
 
+    inline void Pipeline::reserve(unsigned count) throw()
+    {
+        if (_count + count > _capacity)
+        {
+            _capacity += _count + count;
+            Stage** stages = AllocateMemory<Stage*>(_manager->_allocator, _capacity);
+            memcpy(stages, _stages, _count * sizeof(*stages));
+            FreeMemory(_stages);
+        }
+    }
+
+    void Pipeline::addStage(const Stage& stage) throw()
+    {
+        if (!stage.targets)
+        {
+            assert(false);  // must set Stage targets
+            return;
+        }
+
+        Stage* last = nullptr;
+
+        reserve(1);
+
+        switch (stage.type)
+        {
+        case Stage::Type::Clear:
+            last = new(AllocateMemory<ClearStage>(&_manager->_clearStageAllocator, 1)) ClearStage((ClearStage&)stage);
+            break;
+        case Stage::Type::Batch:
+            last = new(AllocateMemory<BatchStage>(&_manager->_batchStageAllocator, 1)) BatchStage((BatchStage&)stage);
+            _portSet |= ((BatchStage&)stage).renderPort->getBit();
+            break;
+        case Stage::Type::Filter:
+            last = new(AllocateMemory<FilterStage>(&_manager->_filterStageAllocator, 1)) FilterStage((FilterStage&)stage);
+            break;
+        default:
+            assert(false);
+            return;
+        }
+
+        AddRef(last->targets);
+
+        _stages[_count] = last;
+        _count++;
+    }
+
 }
---
+++ source/code/render/Stage.h
@@ -0,0 +1,130 @@
+#pragma once
+
+#include "TargetSet.h"
+#include "RenderPort.h"
+#include "core/types.h"
+
+namespace eigen
+{
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Stage
+    //
+    // A phase of the rendering pipeline, specifying the output targets and operations on them.
+    //
+    // There are three types:
+    //
+    // - ClearStage clears the targets
+    // - BatchStage renders all batches received by a given RenderPort
+    // - FilterStage invokes a shader
+    //
+
+    struct Stage
+    {
+        enum class Type
+        {
+            Unspecified = 0,
+            Clear,
+            Batch,
+            Filter,
+        };
+
+        TargetSet*              targets = nullptr;
+        Type                    type    = Type::Unspecified;
+
+    protected:
+                                Stage() {}
+    };
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // ClearStage
+    //
+
+    struct ClearStage : public Stage
+    {
+        enum class Flags
+        {
+            None                = 0,
+            Color               = (1 << 0),
+            Depth               = (1 << 1),
+            Stencil             = (1 << 2),
+
+            Color_Depth_Stencil = (Color | Depth | Stencil),
+            Color_Depth         = (Color | Depth          ),
+            Color_Stencil       = (Color |         Stencil),
+            Depth_Stencil       = (        Depth | Stencil),
+        };
+
+                                ClearStage();
+
+        ClearStage&             setColor(int target, const Float4& color);
+        ClearStage&             setDepth(float depth);
+        ClearStage&             setStencil(unsigned stencil);
+        ClearStage&             setFlags(Flags flags);
+
+        Float4                  colors[TargetSet::MaxTextures];
+        float                   depth   = 0.f;
+        unsigned                stencil = 0;
+        Flags                   flags   = Flags::None;
+    };
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // BatchStage
+    //
+
+    struct BatchStage         : public Stage
+    {
+        enum SortType
+        {
+            Default           = 0,
+            Performance       = Default,
+            IncreasingDepth,
+            DecreasingDepth,
+            Count
+        };
+                                BatchStage();
+
+        BatchStage&             setRenderPort(RenderPort* port);
+        BatchStage&             setSortType(SortType sortType);
+
+        RenderPort*             renderPort;
+        SortType                sortType;
+    };
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // FilterStage
+    //
+
+    struct FilterStage        : public Stage  // a.k.a. quad stage
+    {
+        // TODO
+                                FilterStage();
+        //Shader*               shader;
+    };
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    EIGEN_DEFINE_ENUM_BIT_OPS(ClearStage::Flags)
+
+    inline ClearStage::ClearStage()
+    {
+        type = Type::Clear;
+    }
+
+    inline BatchStage::BatchStage()
+    {
+        type = Type::Batch;
+    }
+
+    inline FilterStage::FilterStage()
+    {
+        type = Type::Filter;
+    }
+
+}
